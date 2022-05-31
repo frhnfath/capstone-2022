0c39128cdb5eb7d1b753bee6bc1eb3732a50b647
Farhan Fathurrahman <frhnfath_farhan@apps.ipb.ac.id>
2022-05-31 17:59:22 +0700

    second commit, register activity and splashscreen

--- app/src/main/java/com/frhnfath/ambuhelp/LoginActivity.kt
+++ app/src/main/java/com/frhnfath/ambuhelp/LoginActivity.kt
@@ -1,11 +1,28 @@
 package com.frhnfath.ambuhelp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.frhnfath.ambuhelp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // function to change activity
+        binding.tvMethod.setOnClickListener {
+            val intent = Intent(this, GoogleMethodsActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.registerTv.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
